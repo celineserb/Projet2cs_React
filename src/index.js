5dc9e0dfa56652ee68a5e37c8b431b63f44fa422
mayalb <hm_larbi@esi.dz>
2021-05-07 23:46:36 +0100

    login page

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
+import 'react-app-polyfill/ie11'; // For IE 11 support
+import 'react-app-polyfill/stable';
+import 'core-js';
+import './polyfill'
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import { PersistGate } from "redux-persist/integration/react";
-import { Routes } from "./app/routes";
-import { persistor, store } from "./config";
-import './index.scss'
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+import { icons } from './assets/icons'
+
+import { Provider } from 'react-redux'
+import store from './store'
+
+React.icons = icons
+
 ReactDOM.render(
-     <Provider store={store}>
-      <PersistGate persistor={persistor} loading={"Loading..."}>
-        <BrowserRouter basename={'/'}>
-          <Routes />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>,
+  <Provider store={store}>
+    <App/>
+  </Provider>,
   document.getElementById('root')
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
