c6f70d4825feea7602a5ab45266a3266dd6d3ae7
TwoToesUp <outsiderstuff@gmail.com>
2021-05-15 10:18:33 +0100

    Modified File Structure

--- src/app/pages/PrivatePages/VehicleSurveillance/index.js
+++ src/app/pages/PrivatePages/VehicleSurveillance/index.js
@@ -3,20 +3,15 @@ import React from 'react'
 import {Row, Col, Layout} from 'antd';
 import 'antd/dist/antd.css';
 
-import  SideBar from './Sidebar/SideMenu';
-import Mappe from './Map/Map.js'
-import TenantInfo from './Tracking/TenantInfo'
-import VehicleState from './Tracking/VehicleState'
-
-import SideMenu from './Sidebar/SideMenu'
-import SuiviPage from './SuiviPage'
+import SideBar from './Sidebar/SideBar'
+import TrackingPage from './TrackingPage'
 
 const { Header, Content, Footer, Sider} = Layout;
 function MainPage() {
     return ( 
         <Layout>
-            <SideMenu></SideMenu>
-            <SuiviPage></SuiviPage>
+            <SideBar></SideBar>
+            <TrackingPage></TrackingPage>
         </Layout>
     );
 }
