e1979e9ce794fee641121d4f15c5e44c75156643
TwoToesUp <oustsiderstuff@gmail.com>
2021-05-16 23:44:59 +0100

    updated project structure

--- src/app/pages/PrivatePages/VehicleSurveillance/TrackingPage.js
+++ src/app/pages/PrivatePages/VehicleSurveillance/TrackingPage.js
@@ -5,7 +5,9 @@ import 'antd/dist/antd.css';
 import VehicleState from './Tracking/VehicleState';
 import TenantInfo from './Tracking/TenantInfo';
 import Mappe from './Map/Map';
-import TopBar from './Topbar/Topbar';
+import TopBar from '../common/Topbar/Topbar';
+
+import SideBar from '../common/Sidebar/Sidebar'
 
 const { Content, Footer} = Layout;
 
@@ -13,26 +15,31 @@ class   TrackingPage extends Component {
 
     render() { 
         return (  
+          <Layout className="site-layout">
+            <SideBar></SideBar>
             <Layout>
-            <Content >
-            <TopBar></TopBar>
-            <div className="site-layout-background" style={{ paddingTop: 30, paddingLeft:40, paddingBottom: 30, minHeight: 500 }}>
-                <Mappe></Mappe>
-            </div>
-              <div className="site-layout-background" style={{ minHeight: 240 }}>
-                <Row>
-                    <Col  xl={10}  lg={10} md={8} sm={24} xs={24}>
-                     <TenantInfo></TenantInfo>
-                    </Col>  
-                    <Col xl={14} lg={14} md={16} sm={24} xs={24}>
-                      <VehicleState></VehicleState>
-                    </Col>
-                </Row>
-                
+              <Content >
+              <TopBar></TopBar>
+              <div className="site-layout-background" style={{ paddingTop: 30, paddingLeft:40, paddingBottom: 30, minHeight: 500 }}>
+                  <Mappe></Mappe>
               </div>
-            </Content>
-            <Footer style={{ textAlign: 'center' }}>Copyright ©2021</Footer>
+                <div className="site-layout-background" style={{ minHeight: 240 }}>
+                  <Row>
+                      <Col  xl={10}  lg={10} md={8} sm={24} xs={24}>
+                      <TenantInfo></TenantInfo>
+                      </Col>  
+                      <Col xl={14} lg={14} md={16} sm={24} xs={24}>
+                        <VehicleState></VehicleState>
+                        <br></br>
+                      </Col>
+                  </Row>
+                  
+                </div>
+              </Content>
+              <Footer style={{ textAlign: 'center' }}>Copyright ©2021</Footer>
+            </Layout>
           </Layout>
+            
         );
     }
 }
