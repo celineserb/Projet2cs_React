f12d0b7a23d450f886827422731786dfb925345c
safirihani <gs_rihani@esi.dz>
2021-05-16 19:10:08 +0100

    feat: vehicle page number

--- src/app/pages/PrivatePages/VehicleManage/ManagePage.js
+++ src/app/pages/PrivatePages/VehicleManage/ManagePage.js
@@ -10,8 +10,9 @@ import './style/VehicleList.css';
 const ManagePage = () =>{
     const [nbPage, setNbPage] = useState(0);
     const [nbOfPages, setNbOfPages] = useState(0);
-    const [nbVehiculesPerPage, setNbVehiculesPerPage] = useState(20);
+    const [nbVehiculesPerPage, setNbVehiculesPerPage] = useState(5);
     const [vehicles, setVehicles] = useState([]);
+    const [nbVehicles, setNbVehicles] = useState(0);
 
     // fetch data on mount component
     useEffect(() => {
@@ -21,9 +22,21 @@ const ManagePage = () =>{
         .then(({nbVehicles, nbPages, listVehicles}) => {
             setVehicles(listVehicles);
             setNbOfPages(nbPages);
+            setNbVehicles(nbVehicles);
          })
      
     }, [nbPage]);
+
+    const previousPage = (nbPage, setNbPage) => {
+        if(nbPage != 0) setNbPage(nbPage - 1);
+        else setNbPage(0);
+    }
+    
+    
+    const nextPage = (nbPage, setNbPage, nbPages) => {
+        if(nbPage < nbPages) setNbPage(nbPage + 1);
+        else setNbPage(nbPages - 1);
+    }
 
     return(
         <div className='list-container'>
@@ -45,12 +58,12 @@ const ManagePage = () =>{
                 }
             </ul>
             <div className='pagination'>
-                <p>Lignes par page: </p>
+                <p>Lignes par page: {nbVehiculesPerPage}</p>
                 
-                <p>1-{nbVehiculesPerPage} of {vehicles.length}</p>
+                <p>{1 + nbVehiculesPerPage * nbPage}-{nbVehiculesPerPage * (nbPage + 1)} of {nbVehicles}</p>
                 <div className='arrows'>
-                    <button><i className='arrow left'></i></button>
-                    <button><i className='arrow right'></i></button>
+                    <button onClick={() => setNbPage(nbPage - 1)} ><i className='arrow left'></i></button>
+                    <button onClick={() => setNbPage(nbPage + 1)} ><i className='arrow right'></i></button>
                 </div>
             </div>
         </div>
