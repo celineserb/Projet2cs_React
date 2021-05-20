cb990afead0c9f4521c4b4c58484508f741caf27
celineserb <38558430+celineserb@users.noreply.github.com>
2021-05-20 23:18:14 +0100

    Revert "login page"
    This reverts commit 5dc9e0dfa56652ee68a5e37c8b431b63f44fa422.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,19 @@
-import 'react-app-polyfill/ie11'; // For IE 11 support
-import 'react-app-polyfill/stable';
-import 'core-js';
-import './polyfill'
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-import { icons } from './assets/icons'
-
-import { Provider } from 'react-redux'
-import store from './store'
-
-React.icons = icons
-
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { Routes } from "./app/routes";
+import { persistor, store } from "./config";
+import './index.scss'
 ReactDOM.render(
-  <Provider store={store}>
-    <App/>
-  </Provider>,
+     <Provider store={store}>
+      <PersistGate persistor={persistor} loading={"Loading..."}>
+        <BrowserRouter basename={'/'}>
+          <Routes />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
