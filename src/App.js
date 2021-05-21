404da2f787e938e193249f0c685b1894a296e8fb
BillelBenoudjit <hb_benoudjit@esi.dz>
2021-05-21 15:17:27 +0100

    Integrate authentication logic

--- src/App.js
+++ src/App.js
@@ -1,63 +1,57 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch ,Link } from 'react-router-dom';
-import './scss/style.scss';
-// import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import "./scss/style.scss";
+
+import Login from "./views/pages/login/Login";
+import TheLayout from "./containers/TheLayout";
 
 const loading = (
   <div className="pt-3 text-center">
     <div className="sk-spinner sk-spinner-pulse"></div>
   </div>
-)
-
-// Containers
-const TheLayout = React.lazy(() => import('./containers/TheLayout'));
-
-// Pages
-const Login = React.lazy(() => import('./views/pages/login/Login'));
-const Register = React.lazy(() => import('./views/pages/register/Register'));
-const Page404 = React.lazy(() => import('./views/pages/page404/Page404'));
-const Page500 = React.lazy(() => import('./views/pages/page500/Page500'));
-
-const isAuthenticated = () => {
-  //write your condition here
-  return false;
-}
-
-
-const UnauthenticatedRoute = ({ component: Component, ...rest }) => (
-  <Route {...rest} render={(props) => (
-    !isAuthenticated()
-      ? <Component {...props} />
-      : <Link to="/login">Back to home page</Link>
-  )} />
 );
 
-
-// const AuthenticatedRoute = ({ component: Component, ...rest }) => (
-//   <Route {...rest} render={(props) => (
-//     isAuthenticated()
-//       ? <Component {...props} />
-//       : <Redirect to='/login' />
-//   )} />
-// );
-
+const PrivateRoute = ({ children, ...rest }) => (
+  <Route
+    {...rest}
+    render={({ location }) =>
+      localStorage.getItem("token") ? (
+        children
+      ) : (
+        <Redirect
+          to={{
+            pathname: "/",
+            state: { from: location },
+          }}
+        />
+      )
+    }
+  />
+);
 
 class App extends Component {
-
   render() {
     return (
-      <BrowserRouter>
-          <React.Suspense fallback={loading}>
-            <Switch>
-            {/* <UnauthenticatedRoute exact path="/login" name="Login Page" component={Login} /> */}
-              <Route exact path="/" name="Login Page"  render={props => <Login {...props}/>} />
-              <Route exact  path="/register"   name="Register Page" render={props => <Register {...props}/>} />
-              <Route exact path="/404"  name="Page 404"  render={props => <Page404 {...props}/>} />
-              <Route exact  path="/500"  name="Page 500"   render={props => <Page500 {...props}/>} />
-              <Route  path="/" name="Home" render={props => <TheLayout {...props}/>} />
-            </Switch>
-          </React.Suspense>
-      </BrowserRouter>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route
+              exact
+              path="/"
+              name="Login Page"
+              render={(props) => <Login {...props} />}
+            />
+            <PrivateRoute path="/dashboard">
+              <TheLayout />
+            </PrivateRoute>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
--- src/containers/TheLayout.js
+++ src/containers/TheLayout.js
@@ -1,25 +1,21 @@
-import React from 'react'
-import {
-  TheContent,
-  TheSidebar,
-  TheFooter,
-  TheHeader
-} from './index'
+import React from "react";
+import { TheContent, TheSidebar, TheFooter, TheHeader } from "./index";
 
-const TheLayout = () => {
+import { withRouter } from "react-router-dom";
 
+const TheLayout = (props) => {
   return (
     <div className="c-app c-default-layout">
-      <TheSidebar/>
+      <TheSidebar />
       <div className="c-wrapper">
-        <TheHeader/>
+        <TheHeader />
         <div className="c-body">
-          <TheContent/>
+          <TheContent />
         </div>
-        <TheFooter/>
+        <TheFooter />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TheLayout
+export default withRouter(TheLayout);
