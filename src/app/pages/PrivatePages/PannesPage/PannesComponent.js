613787c2ecf37e35273a504735ce065aaf61007e
Ouarab <hy_ouarab@esi.dz>
2021-05-22 17:58:39 +0100

    refactor: changed PannesPage structure to conform with others

--- src/app/pages/PrivatePages/PannesPage/PannesComponent.js
+++ src/app/pages/PrivatePages/PannesPage/PannesComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import "./style.scss";
+import "./style/style.scss";
 
 var data = [
     {
@@ -107,61 +107,61 @@ function showMore(i) {
     if (msg.classList.contains("truncated")) {
         msg.classList.remove("truncated");
         p.classList.remove("truncated");
-        btn.innerHTML = '<img src="media/chevron-bottom.svg" alt="" />';
+        btn.innerHTML = '<img class="chevron-bottom" alt="" />';
     } else {
         msg.classList.add("truncated");
         p.classList.add("truncated");
-        btn.innerHTML = '<img src="media/chevron-right.svg" alt="" />';
+        btn.innerHTML = '<img class="chevron-right" alt="" />';
     }
 }
 
 function PannesComponent() {
     return (
-        <div class="pannes-list-wrapper">
-			<div class="pannes-list-header">
-				<label class="pannes-list-title">Pannes</label>
-				<button class="pannes-list-sort-btn" onClick={() => {
+        <div className="pannes-list-wrapper">
+			<div className="pannes-list-header">
+				<label className="pannes-list-title">Pannes</label>
+				<button className="pannes-list-sort-btn" onClick={() => {
                     const sort = document.getElementsByClassName("pannes-list-sort")[0];
                     if (sort.classList.contains("hidden")) {
                         sort.classList.remove("hidden");
                     } else {
                         sort.classList.add("hidden");
                     }
-                }}><img src="media/sort.svg" alt="" /> Ordonner</button>
-				<div class="pannes-list-sort hidden">
+                }}><img className="sort-svg" alt="" /> Ordonner</button>
+				<div className="pannes-list-sort hidden">
 					<ul>
 						<li><input type="checkbox" id="sort-latest" /><label for="sort-latest">Plus récents</label></li>
 						<li><input type="checkbox" id="sort-unseen" /><label for="sort-unseen">Non vues</label></li>
 					</ul>
 				</div>
-				<div class="hl"></div>
+				<div className="hl"></div>
 			</div>
-			<div class="pannes-list-body">
+			<div className="pannes-list-body">
                 {
                     data.map((e, i) =>
-                    <div class="panne-notif-wrapper">
+                    <div className="panne-notif-wrapper">
                         {
-                            e.seen? <div class="panne-notif-seen"><div></div></div>
-                            : <div class="panne-notif-unseen"><div></div></div>
+                            e.seen? <div className="panne-notif-seen"><div></div></div>
+                            : <div className="panne-notif-unseen"><div></div></div>
                         }
-                        <div class="panne-notif-body">
-                            <div class="panne-notif-message truncated">
-                                <p class="truncated">{e.message}</p>
+                        <div className="panne-notif-body">
+                            <div className="panne-notif-message truncated">
+                                <p className="truncated">{e.message}</p>
                             </div>
-                            <div class="panne-notif-source truncated">
+                            <div className="panne-notif-source truncated">
                                 <div>
-                                    <label class="truncated">{e.vehicle}</label><br /> 
-                                    <label class="truncated">{e.source}</label>
+                                    <label className="truncated">{e.vehicle}</label><br /> 
+                                    <label className="truncated">{e.source}</label>
                                 </div>
                             </div>
-                            <div class="panne-notif-date truncated">
+                            <div className="panne-notif-date truncated">
                                 <div>
-                                    <label class="truncated">{e.date}</label><br />
-                                    <label class="truncated">{e.time}</label>
+                                    <label className="truncated">{e.date}</label><br />
+                                    <label className="truncated">{e.time}</label>
                                 </div>
                             </div>
-                            <div class="panne-notif-more">
-                                <button id={"more"+i} onClick={() => {showMore(i)}}><img src="media/chevron-right.svg" alt="" /></button>
+                            <div className="panne-notif-more">
+                                <button id={"more"+i} onClick={() => {showMore(i)}}><img className="chevron-right" alt="" /></button>
                             </div>
                         </div>
                     </div>
