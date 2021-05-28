be1226286745f110da264263d4affb152f056c1a
nabil-otsmane <hn_otsmane@esi.dz>
2021-05-28 22:18:03 +0100

    feat: fixed login design to match prototype

--- src/app/pages/AuthPages/login/Login.js
+++ src/app/pages/AuthPages/login/Login.js
@@ -11,6 +11,7 @@ import {
   CInputGroup,
   CInputGroupPrepend,
   CInputGroupText,
+  CLink,
   CRow,
 } from "@coreui/react";
 import CIcon from "@coreui/icons-react";
@@ -19,6 +20,11 @@ import { login } from '../../../../modules/Auth/auth.crud'
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router";
 import { actions } from "../../../../modules";
+import Logo from "../../../../assets/images/logo_yellow.png"
+import Logo2 from "../../../../assets/images/logo_gris.png"
+import User from '../../../../assets/images/user.png'
+import Lock from '../../../../assets/icons/lock.png'
+import "../../../../assets/scss/login.css"
 
 const Login = () => {
   const [state, setState] = useState({
@@ -83,20 +89,43 @@ const Login = () => {
   }
 
   return (
-    <div className="c-app c-default-layout flex-row align-items-center">
-      <CContainer>
-        <CRow className="justify-content-center">
-          <CCol md="8">
+    <div className="c-app c-default-layout flex-row align-items-stretch">
+      <CContainer className="mr-md-5">
+        <CCol className="d-flex flex-column fixed-top pl-5">
+          <CLink className="small" to="/">
+            <CRow className="d-flex pt-md-5 align-items-center">
+              <CIcon src={Logo2} height={40} />
+              <p className="m-0 ml-3">AutoLib Dz</p>
+            </CRow>
+          </CLink>
+          <div className="big">
+            <div>
+              <h3>Louer une voiture</h3><br />
+              <h1>Librement et rapidement</h1>
+            </div>
+            <div>
+              <p>Nous vous offrons les meilleurs services<br />Inscriver vous, c'est gratuit!</p>
+            </div>
+          </div>
+          <div className="small">
+            <CButton color="primary">Savoir plus</CButton>
+          </div>
+
+        </CCol>
+        <CRow className="h-100 justify-content-end align-items-end">
+          <CCol sm="5" className="mb-5">
             <CCardGroup>
               <CCard className="p-4">
                 <CCardBody>
-                  <CForm>
-                    <h1>Login</h1>
-                    <p className="text-muted">Sign In to your account</p>
-                    <CInputGroup className="mb-3">
+                  <CForm className="d-flex h-100 flex-column justify-content-between">
+                    <CRow className="justify-content-center">
+                      <CIcon className="align-self-center" src={Logo} height={100} />
+                    </CRow>
+                      <h1>Se connecter a votre compte</h1>
+                    <CInputGroup className="mb-3 p-2">
                       <CInputGroupPrepend>
                         <CInputGroupText>
-                          <CIcon name="cil-user" />
+                          <CIcon src={User} height={25} />
                         </CInputGroupText>
                       </CInputGroupPrepend>
                       <CInput
@@ -107,10 +136,10 @@ const Login = () => {
                         onChange={handleChange}
                       />
                     </CInputGroup>
-                    <CInputGroup className="mb-4">
+                    <CInputGroup className="mb-4 p-2">
                       <CInputGroupPrepend>
                         <CInputGroupText>
-                          <CIcon name="cil-lock-locked" />
+                          <CIcon src={Lock} height={25} />
                         </CInputGroupText>
                       </CInputGroupPrepend>
                       <CInput
@@ -122,15 +151,15 @@ const Login = () => {
                       />
                     </CInputGroup>
                     <CRow>
-                      <CCol xs="6">
+                      <CCol>
                         <CButton
                           color="primary"
-                          className="px-4"
+                          className="login px-5"
                           active
                           tabIndex={-1}
                           onClick={handleSubmitClick}
                         >
-                          Login
+                          Se connecter
                         </CButton>
                       </CCol>
                     </CRow>
