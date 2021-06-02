c208b1357c58eeb626c4855577e106c1ad76abbc
TwoToesUp <outsiderstuff@gmail.com>
2021-06-02 06:29:18 +0100

    refactor: signal details in theft signal page style

--- src/services/crud.services.js
+++ src/services/crud.services.js
@@ -4,7 +4,7 @@ const URL_ROOT="http://localhost";
 export const Services = {
     AUTH_URL: `${URL_ROOT}:8005`,
     USER_URL: `${URL_ROOT}:8100`,
-    TRACKING_URL: `${URL_ROOT}:8001`,
+    TRACKING_URL: `${URL_ROOT}:8003`,
     GEOLOC_URL: `${URL_ROOT}:8002`,
     VEHICLE_URL: `${URL_ROOT}:8200`,
     STAT_URL: `${URL_ROOT}:8089`
