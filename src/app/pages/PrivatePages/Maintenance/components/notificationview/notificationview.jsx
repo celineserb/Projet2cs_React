541f0958666051cbb2981c1e7ab7f94a399d36c5
Nerostarx <35573892+NeroStarx@users.noreply.github.com>
2021-06-05 00:11:32 +0100

    finish: importing a panne notif into a task

--- src/app/pages/PrivatePages/Maintenance/components/notificationview/notificationview.jsx
+++ src/app/pages/PrivatePages/Maintenance/components/notificationview/notificationview.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react'
 import Button from '../button/button'
 import Modal from 'react-modal'
+import axios from 'axios'
 import './notificationview.css'
 
 
@@ -8,6 +10,39 @@ Modal.setAppElement("#root")
 export default function NotificationView(props){
     const panne = props.panne
     const [isModalOpen, setModalOpen] = useState(false)
+    const [taskModels,setTaskModels] = useState([])
+    const [selectedTaskModel, setSelectedModel] = useState(0)
+    const [selectedAgent,setSelectedAgent] = useState(100)
+
+    useEffect(async ()=>{
+        const result = await axios("https://service-tasks.herokuapp.com/taskModel")
+        setTaskModels(result.data)
+    }, [])
+
+
+    async function importNotif(){
+        if(selectedTaskModel !== 0 && selectedAgent !== 0){
+            const result = await axios.post("https://service-tasks.herokuapp.com/task"
+            ,{
+                "idAgent": 100,
+                "idVehicle": panne.idVehicle,
+                "taskTitle": panne.signalType,
+                "description": panne.message,
+                "idTaskState": 2,
+                "endDate": null,
+                "idTaskModel": selectedTaskModel,
+                "usedEquipments": null
+            })
+
+            console.log(result)
+
+            if(result.status === 200){
+                setModalOpen(!isModalOpen)
+                setSelectedModel(0)
+                setSelectedAgent(100)
+            }
+        }
+    }
 
     return(
         <div className="notification-view-container">
@@ -19,7 +54,7 @@ export default function NotificationView(props){
             <p className="notification-subtitle">
                 {new Date(panne.sent_at).toISOString().slice(0, 10)}
             </p>
-            <Button text="Détails" mode="light_mode" onClick={()=>{setModalOpen(!isModalOpen)}}/>
+            <Button text="importer" mode="light_mode" onClick={()=>{setModalOpen(!isModalOpen)}}/>
             <Modal
                 isOpen={isModalOpen}
                 contentLabel="Notification details"
@@ -43,12 +78,25 @@ export default function NotificationView(props){
                 <div className="notif-modal-wrapper">
                     <p className="bold-title notif-modal-title">Détails de la panne</p>
                     <p className="notif-modal-text">{"Description: "+panne.message}</p>
-                    <p className="notif-modal-text">{"Source: "+ panne.sourceType}</p>
                     <p className="notif-modal-text">{"Date: "+ new Date(panne.sent_at).toISOString().slice(0, 10)}</p>
                     <p className="notif-modal-text">{"Véhicule: "+panne.vehiclebrand}</p>
+                    <select name="notif-task-model" id="notif-task-model" 
+                            className="modal-form-input" 
+                            placeholder="modèl de tache"
+                            onChange={event => setSelectedModel(event.target.value)}>
+                        {taskModels.map(model => (
+                            <option key={model.id} value={model.id}>{model.taskModelName}</option>
+                        ))}
+                    </select>
+                    <br />
+                    <select name="notif-agent-model" 
+                            id="notif-agent-model"
+                            className="modal-form-input">
+
+                    </select>
                     <div className="notif-buttons-wrapper">
                         <Button text="Confirmer" mode="dark_mode" onClick={()=>{setModalOpen(false)}}/>
-                        <Button text="Importer comme tache" mode="light_mode"/>
+                        <Button text="Importer comme tache" mode="light_mode" onClick={()=>{importNotif()}}/>
                     </div>
                 </div>
             </Modal>
