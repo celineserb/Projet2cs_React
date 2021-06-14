5d00b540c05f9d61c4d527eeb34e3ae1811a9c2c
fejzann <gs_rihani@esi.dz>
2021-06-14 12:43:35 +0100

    feat : add notification service

--- src/app/pages/PrivatePages/common/Topbar/Topbar.js
+++ src/app/pages/PrivatePages/common/Topbar/Topbar.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component, useState, useEffect } from 'react'
+import axios from 'axios';
 import { useDispatch} from 'react-redux'
 
 import {Button, Dropdown, Menu, Avatar} from 'antd';
@@ -12,24 +13,67 @@ import {ReactComponent as UserIcon } from '../../../../../assets/svg/user.svg'
 
 import {actions} from '../../../../../modules'
 import { useLocation } from 'react-router';
-
+import Notifications, {notify} from 'react-notify-toast';
+import socketIOClient from "socket.io-client";
 
 
 export default function TopBar(props) {
+    const [notifications, setNotifications] = useState();
 
     const menu = (
         <Menu>
-          <Menu.Item key="0">
-              1st  item
-          </Menu.Item>
-          <Menu.Item key="1">
-              2nd  item
-          </Menu.Item>
-          <Menu.Item key="3" >
-            3rd item
-          </Menu.Item>
+            {
+                notifications?.map((item, key) =>{
+                    return(
+                        <Menu.Item key={key}
+                        style={{
+                            maxWidth:250,
+                            whiteSpace: "nowrap",
+                            overflow: "hidden",
+                            textOverflow: "ellipsis"
+                        }}>
+                            <Avatar 
+                                  size={{
+                                    xs:8,
+                                    sm: 8,
+                                    md: 12,
+                                    lg: 12,
+                                    xl: 12,
+                                    xxl: 12,
+                                }}
+                                style={{
+                                    backgroundColor:"red"
+                                }}
+                             />
+                           {"   "+item.read} 
+                           
+                        </Menu.Item>
+                    )
+                }
+               
+            )}
         </Menu>
       );
+
+
+    useEffect(() =>{
+        axios.get('http://localhost:8004/breakdown')
+            .then((res) => {
+                console.log(res);
+                const unreadNotif = res.data.filter((notification) => notification.read == false);
+                console.log(unreadNotif);
+                setNotifications(unreadNotif);
+            });
+
+        // listen to notifications events
+
+        const socket = socketIOClient("http://localhost:8004");
+        socket.on("messageSent", (message) => {
+            notify.show(message.message, "success", 5000);
+        });
+
+    }, []);
+
      
     
     const disconnect =(
@@ -63,6 +107,10 @@ export default function TopBar(props) {
     
     const dispatch = useDispatch()
 
+    // waiting for any notification
+    // change the notification url (usually the port)
+
+
     function logout(){
         dispatch(actions.logout());
     }
@@ -71,6 +119,7 @@ export default function TopBar(props) {
 
         <div className="topbar-wrapper">
         <div className="back-wrapper">
+            <Notifications />
             <a className="back-link" href="#">  
                 <div className="back-text">
                 { 
@@ -109,7 +158,7 @@ export default function TopBar(props) {
                     <div className="notifs-wrapper">
                         <div className="notifs-icon">
                         <Dropdown 
-                            overlay={menu}
+                        overlay={menu}
                             placement="bottomRight"
                         >
                             <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
